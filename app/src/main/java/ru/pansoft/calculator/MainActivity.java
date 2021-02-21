4bb009b7c784efbfe9692b46d070b26c380003ba
pansoftpansoft <pansoft@bk.ru>
2021-02-21 22:29:13 +0300

    Lesson1

--- app/src/main/java/ru/pansoft/calculator/MainActivity.java
+++ app/src/main/java/ru/pansoft/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package ru.pansoft.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        // создаем обработчик нажатия
+        View.OnClickListener btnNext = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                setContentView(R.layout.activity_main2);
+            }
+        };
+        // присвоим обработчик кнопке OK (btnOk)
+        buttonNext.setOnClickListener(btnNext);
     }
+
 }
