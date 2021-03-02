6cd5ebec5fbff8148da5d62c34fab964a00b87b8
pansoftpansoft <pansoft@bk.ru>
2021-03-02 21:50:24 +0300

    update

--- app/src/main/java/ru/pansoft/calculator/MainActivity.java
+++ app/src/main/java/ru/pansoft/calculator/MainActivity.java
@@ -3,8 +3,6 @@ package ru.pansoft.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
